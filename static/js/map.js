ab5b763384d5754debcf4d1b9047369c7626665a
Glenn Carremans <glenn.carremans@appwise.be>
2017-07-09 14:08:12 +0200

    Updated raid icons

--- static/js/map.js
+++ static/js/map.js
@@ -498,7 +498,7 @@ function gymLabel(item) {
         raidStr += '<div style="margin-bottom: 10px">Time: <b>' + raidStartStr + '</b> - <b>' + raidEndStr + '</b></div>'
 
         if (raidStarted) {
-            raidIcon = '<i class="pokemon-large-sprite n' + item.raid_pokemon_id + '"></i>'
+            raidIcon = '<i class="pokemon-large-raid-sprite n' + item.raid_pokemon_id + '"></i>'
         } else {
             var raidEgg = ''
             if (item['raid_level'] <= 2) {
@@ -1958,7 +1958,7 @@ function showGymDetails(id) { // eslint-disable-line no-unused-vars
             raidStr += '<div style="margin-bottom: 10px">Time: <b>' + raidStartStr + '</b> - <b>' + raidEndStr + '</b></div>'
 
             if (raidStarted) {
-                raidIcon = '<i class="pokemon-large-sprite n' + result.raid_pokemon_id + '"></i>'
+                raidIcon = '<i class="pokemon-large-raid-sprite n' + result.raid_pokemon_id + '"></i>'
             } else {
                 var raidEgg = ''
                 if (result['raid_level'] <= 2) {
