4a052020d9b2b1f8dd9c84699f91c53f66bf1b36
Glenn Carremans <glenn.carremans@appwise.be>
2017-09-20 20:55:08 +0200

    Fixed stupid typo...

--- static/js/map.js
+++ static/js/map.js
@@ -837,7 +837,7 @@ function getGymMarkerIcon(item) {
     if (item['raid_pokemon_id'] !== null && item.raid_end > Date.now()) {
         return '<div style="position:relative;">' +
             '<img src="static/forts/' + Store.get('gymMarkerStyle') + '/' + teamStr + '.png" style="width:55px;height:auto;"/>' +
-            '<i class="pokemon-raid-sprite n' + item.graid_pokemon_id + '"></i>' +
+            '<i class="pokemon-raid-sprite n' + item.raid_pokemon_id + '"></i>' +
             '</div>'
     } else if (item['raid_level'] !== null && item.raid_end > Date.now()) {
         var raidEgg = ''
