43aeb2e685b702929a7b8d3189a94cdddfd021e3
James Harland <me@jamesharland.com>
2018-01-24 14:31:21 +0000

    Linting fixes

--- static/js/map.common.js
+++ static/js/map.common.js
@@ -1135,10 +1135,11 @@ function getGoogleSprite(index, sprite, displayHeight, weather = 0) {
     var scaledIconOffset = new google.maps.Point(index % sprite.columns * sprite.iconWidth * scale + 0.5, Math.floor(index / sprite.columns) * sprite.iconHeight * scale + 0.5)
     var scaledSpriteSize = new google.maps.Size(scale * sprite.spriteWidth, scale * sprite.spriteHeight)
     var scaledIconCenterOffset = new google.maps.Point(scale * sprite.iconWidth / 2, scale * sprite.iconHeight / 2)
+    var monSpriteUrl
     if (weather === 0) {
-        var monSpriteUrl = Store.get('spritefileLarge')
+        monSpriteUrl = Store.get('spritefileLarge')
     } else {
-        var monSpriteUrl = Store.get('weatherSpritesSrc') + weather + '.png'
+        monSpriteUrl = Store.get('weatherSpritesSrc') + weather + '.png'
     }
     return {
         url: monSpriteUrl,
