40eb825410f097bf55e9440120ff0753cd9ee27b
James Harland <me@jamesharland.com>
2018-01-28 21:09:35 +0000

    Changed operator to ===

--- weather_data.php
+++ weather_data.php
@@ -3,16 +3,16 @@ include('config/config.php');
 global $map, $fork;
 header('Content-Type: application/json');
 // init map
-if (strtolower($map) == "monocle") {
-    if (strtolower($fork) == "asner") {
+if (strtolower($map) === "monocle") {
+    if (strtolower($fork) === "asner") {
         $scanner = new \Scanner\Monocle_Asner();
-    } elseif (strtolower($fork) == "alternate") {
+    } elseif (strtolower($fork) === "alternate") {
         $scanner = new \Scanner\Monocle_Alternate();
     } else {
         $scanner = new \Scanner\Monocle();
     }
-} elseif (strtolower($map) == "rm") {
-    if (strtolower($fork) == "sloppy") {
+} elseif (strtolower($map) === "rm") {
+    if (strtolower($fork) === "sloppy") {
         $scanner = new \Scanner\RocketMap_Sloppy();
     } else {
         $scanner = new \Scanner\RocketMap();
