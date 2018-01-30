67e2279e9006502cebad341d73eaffe028e45302
Daniel Cheng (鄭郁邦) <j16sdiz@users.noreply.github.com>
2018-01-30 13:21:41 +0800

    Fix Monocle/Alternative out of memory with KEEP_GYM_HISTORY
    When `KEEP_GYM_HISTORY` is on in Monocle/Alternative config, the `fort_sighting` and `raid` may contain multiple records of the same gym. Only the latest gym / fort status should be retrieved to avoid out of memory problem.

--- lib/Monocle_Alternate.php
+++ lib/Monocle_Alternate.php
@@ -216,7 +216,8 @@ class Monocle_Alternate extends Monocle
     {
         global $db;
 
-        $query = "SELECT f.external_id AS gym_id,
+        $query = "SELECT
+        f.external_id AS gym_id,
         fs.last_modified AS last_modified,
         updated AS last_scanned,
         f.lat AS latitude,
@@ -235,10 +236,11 @@ class Monocle_Alternate extends Monocle
         r.move_1 AS raid_pokemon_move_1,
         r.move_2 AS raid_pokemon_move_2
         FROM forts f
-        LEFT JOIN fort_sightings fs ON fs.fort_id = f.id
-        LEFT JOIN raids r ON r.fort_id = f.id
+        LEFT JOIN fort_sightings fs ON (fs.fort_id = f.id AND fs.last_modified = (SELECT MAX(last_modified) FROM fort_sightings fs2 WHERE fs2.fort_id=f.id))
+        LEFT JOIN raids r ON (r.fort_id = f.id AND r.time_end >= :time)
         WHERE :conditions";
 
+        $query = str_replace(":time", time(), $query);
         $query = str_replace(":conditions", join(" AND ", $conds), $query);
         $gyms = $db->query($query, $params)->fetchAll(\PDO::FETCH_ASSOC);
 
