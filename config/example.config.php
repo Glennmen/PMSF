a4b6bed99c8d0b7b35106cc7b178851cec543fa6
thermoscookies <vincerules@gmail.com>
2018-02-22 14:44:32 -0800

    added $helppage to example.config.php and if statement to pre-index.html

--- config/example.config.php
+++ config/example.config.php
@@ -56,6 +56,10 @@ $paypalUrl = "";                                                    // PayPal do
 
 $discordUrl = "https://discord.gg/INVITE_LINK";                     // Discord URL, leave "" for empty
 
+/* Help link */
+
+$helplink = "";                                 // URL for help page, leave "" for empty
+
 /* Worldopole */
 
 $worldopoleUrl = "";                                                // Link to Worldopole, leave "" for empty
