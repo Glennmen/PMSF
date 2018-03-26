e8d25aadc64d225351461d72e17f67d9bf3cc053
Martin Falk <falken_liten@hotmail.com>
2018-03-26 19:18:09 +0200

    [fix]styleci? ^_^

--- checklogin.php
+++ checklogin.php
@@ -4,16 +4,16 @@ header('Content-Type: application/json');
 $info = array();
 
 if($enableLogin === true){
-	$info['enableLogin'] = true;
-	if (isset($_SESSION['user'])){
-		$info['isLoggedIn'] = true;
-		$info['current_timestamp'] = time();
-		$info['user'] = $_SESSION['user'];
-	} else {
-		$info['isLoggedIn'] = false;
-	}
+    $info['enableLogin'] = true;
+    if (isset($_SESSION['user'])){
+        $info['isLoggedIn'] = true;
+        $info['current_timestamp'] = time();
+        $info['user'] = $_SESSION['user'];
+    } else {
+        $info['isLoggedIn'] = false;
+    }
 } else {
-	$info['enableLogin'] = false;
+    $info['enableLogin'] = false;
 }
 
 print json_encode($info);
